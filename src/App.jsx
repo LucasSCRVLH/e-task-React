c5ec5764651fe67ac42bcb631da3cf6b389e7c78
Lucas Carvalho <blujack11@gmail.com>
2024-09-11 17:48:58 -0300

    Envio dos ajustes

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,66 @@
+import { useState } from "react";
 import { Header } from "./components/Header";
-
 import styles from "./styles/App.module.css";
 
 function App() {
+  const [inputValue, setInputValue] = useState("");
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      description: "Comprar pão",
+      isCompleted: false,
+    },
+  ]);
+
+  function handleClick() {
+    if (!inputValue) {
+      alert("A tarefa precisa de uma descrição");
+    }
+  }
+
   return (
     <>
-      <Header />
-      <main className={styles.container}>
-        <section className={styles['form-container']}>
-          <input type="text" placeholder="Adicione uma nova tarefa" />
-         
-          <button type="button">
-            Cadastrar
-          </button>
-        </section>
-      
+      <div className={styles.app}>
+        <Header />
+        <main className={styles.container}>
+          <section className={styles["form-section"]}>
+            <input
+              type="text"
+              className={styles.input}
+              placeholder="Adicione uma nova tarefa"
+              onChange={(e) => setInputValue(e.target.value)}
+              value={inputValue}
+            />
+            <button className={styles["create-btn"]} onClick={handleClick}>
+              Cadastrar
+            </button>
+          </section>
 
-        <section className={styles['section-task-list']}>
-          <header className={styles['section-task-header']}>
-            <div className={styles.info}>
-              <strong>Tarefas criadas</strong>
-              <span className={styles['count-tasks']}>0</span>
-            </div>
-            <div className={styles.info}>
-              <strong>Concluídas</strong>
-              <span className={styles['count-finisheds']}>0</span>
-            </div>
-          </header>
+          <section className={styles["task-section"]}>
+            <header className={styles["task-header"]}>
+              <div className={styles["div-task"]}>
+                <strong className={styles.strong}>Tarefas criadas</strong>
+                <span className={styles["count-tasks"]}>0</span>
+              </div>
+              <div className={styles["div-task"]}>
+                <strong className={styles.strong}>Concluídas</strong>
+                <span className={styles["count-finisheds"]}>0</span>
+              </div>
+            </header>
+            <div className="empty-state"></div>
 
-          <div className={styles['empty-state']}></div>
+            {tasks.length === 0 && <p>Sem tasks no momento</p>}
 
-          <ul className={styles['task-list']}></ul>
-        </section>
-      
-      </main>
-
-      
+            <ul className={styles["task-list"]}>
+              {tasks.map((task) => (
+                <li key={task.id}>
+                  <strong>{task.description}</strong>
+                </li>
+              ))}
+            </ul>
+          </section>
+        </main>
+      </div>
     </>
   );
 }
